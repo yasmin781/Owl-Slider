e40f98b969ec58e584f113f33460fe3d54484441
Nazra Noor Yasmin <75300926+yasmin781@users.noreply.github.com>
2020-12-10 14:58:30 +0600

    Add files via upload

---
+++ Owl/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        margin: 10,
+        autoplay: true,
+        autoplaytimeout: 1000,
+        loop: true,
+        autoplayhoverpause: true,
+    });
+  });
